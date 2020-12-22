8b6883f54af7592fc71d5c836fd989b6fd9c6161
Ian Clark <ianclark226@gmail.com>
2020-12-22 14:56:05 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Wear a Mask</title>
+</head>
+<body>
+
+<div class="main-section">
+<div class="add-section">
+<form action="">
+<input type="text" name="first_name" placeholder="First Name" required />
+<input type="text" name="last_name" placeholder="Last Name" required />
+<input type="text" name="email" placeholder="Email" required />
+<input type="text" name="store" placeholder="Store" required />
+<input type="text" name="date" placeholder="Date" required />
+<button type="submit">Add &nbsp; <span>&#43;</span></button>
+
+</form>
+</div>
+<div class="slacker-section">
+<input type="checkbox">
+<h2>This is a test</h2>
+<small>na</small>
+</div>
+</div>
+    
+</body>
+</html>
