e2ecbe1afcaa0a99e60083d51128a63c47332095
Ian Clark <ianclark226@gmail.com>
2020-12-29 15:15:26 -0500

    heroku connect

--- db.php
+++ db.php
@@ -9,8 +9,20 @@ try {
     $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
                     $uName, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+    echo('connected');
 }catch(PDOException $e){
   echo "Connection failed : ". $e->getMessage();
 }
 
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
