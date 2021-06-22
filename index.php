9ba8528c84d22e9f197ed91f1b900d1ebfec5f56
Ian Clark <ianclark226@gmail.com>
2021-06-22 12:41:08 -0400

    timer

--- index.php
+++ index.php
@@ -16,6 +16,40 @@ require 'db.php';
 <div class="main-section">
 <h1 class=>Mask Tracker</h1>
 <img src="https://growth-management.alachuacounty.us/Content/images/mask.png" alt="Mask for app" srcset="">
+
+<?php 
+$date = date('2030-01-01');
+$time = date('12:00:00');
+$date_today = $date . ' ' . $time;
+echo "it will run till " .$date_today;
+?>
+<script type="text/javascript">
+var count_id = "<?php echo $date_today; ?>";
+var countDownDate = new Date(count_id).getTime();
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance/(1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance%(1000 * 60 * 60 * 24))/(1000*60*60));
+    var minutes = Math.floor((distance/(1000 * 60 * 60))/(1000*60));
+    var seconds = Math.floor((distance/(1000 * 60))/1000);
+
+        document.getElementById("timer").innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
+
+        if(distance<0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML="EXPIRED";
+        }      
+},1000);
+</script>
+
+<?php 
+echo '<p id="timer" style="font-size:30px;"></p>'
+?>
+<p>This is the countdown</p>
 <p class=>Insert the person that is not following the rules to mask wearing</p>
 <br>
 <div class="add-section">
