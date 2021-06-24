605e872a7ad2980713decc9d6ee394861f416a92
Ian Clark <ianclark226@gmail.com>
2021-06-24 14:48:25 -0400

    cleanup

--- index.php
+++ index.php
@@ -33,9 +33,9 @@ var x = setInterval(function() {
     var distance = countDownDate - now;
 
     var days = Math.floor(distance/(1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance%(1000 * 60 * 60 * 24))/(1000*60*60));
-    var minutes = Math.floor((distance/(1000 * 60 * 60))/(1000*60));
-    var seconds = Math.floor((distance/(1000 * 60))/1000);
+    var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+    var seconds = Math.floor((distance%(1000*60))/1000);
 
         document.getElementById("timer").innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
 
